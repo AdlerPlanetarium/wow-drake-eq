f101261da50656a392fd09ae6ffdb50b31c807fa
mcbouslog <mcbouslog@gmail.com>
2020-12-13 22:37:16 -0600

    Add range input

--- src/components/TermCard.js
+++ src/components/TermCard.js
@@ -1,18 +1,42 @@
+import { useState } from 'react'
 import {
-  Box
+  Box,
+  RangeInput
 } from 'grommet'
 
-function TermCard ({
+const TermCard = ({
+  defaultValue,
+  description,
   displayName,
-  subscript,
+  estimatedMax,
+  estimatedMin,
+  name,
   question,
-  description
-}) {
+  subscript,
+  valueType
+}) => {
+  const [value, setValue] = useState((valueType === 'percentage') ? (defaultValue * 100) : defaultValue)
+
+  const onChange = event => setValue(event.target.value)
+
+  let num
+
+  if (valueType === 'log') {
+    num = Math.pow(10, value)
+  }
+
+  if (valueType === 'percentage') {
+    num = 100 * value
+  }
+
+  const formatNum = Math.round(num) + (valueType === 'percentage' ? '%' : '')
+
   return (
     <Box
+      key={name}
       align='center'
       background='white'
-      height='90vh'
+      height='80vh'
       justify='center'
       pad='large'
     >
@@ -24,8 +48,16 @@ function TermCard ({
         round='large'
       >
         <p>{displayName}<sub>{subscript}</sub></p>
+        <p>{formatNum}</p>
         <p>{question}</p>
         <p>{description}</p>
+        <RangeInput
+          max={(valueType === 'percentage') ? (estimatedMax * 100) : estimatedMax}
+          min={(valueType === 'percentage') ? (estimatedMin * 100) : estimatedMin}
+          step={(valueType === 'quantity') ? '1' : '0.01'}
+          value={value}
+          onChange={onChange}
+        />
       </Box>
     </Box>
   )
